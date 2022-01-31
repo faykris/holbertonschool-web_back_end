c0c390f0502d7cbe938fb074bf61371c41aedae3
faykris <faykris28@gmail.com>
2022-01-31 16:18:11 -0500

    [Add] 1-concat.py: validate with checker

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""1. Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat - return basic notation of str1 + str2"""
+    return str1 + str2
