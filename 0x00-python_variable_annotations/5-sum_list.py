aeac58f3546b50d97425ce9935a619639a3bc44f
faykris <faykris28@gmail.com>
2022-01-31 17:08:17 -0500

    [Add] 5-sum_list.py: validate with checker

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""5. Complex types - list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum_list - return basic notation of sum of list of floats"""
+    return sum(input_list)
