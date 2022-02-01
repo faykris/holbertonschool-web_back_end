27af01c8e730108d2808c9da95781336021ea803
faykris <faykris28@gmail.com>
2022-02-01 14:35:14 -0500

    [Add] README.md, 0-basic_async_syntax.py: validate with checker

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import time
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """wait_random - make delay in range of max_delay"""
     delay = random.uniform(0, max_delay)
     time.sleep(delay)
     return delay
