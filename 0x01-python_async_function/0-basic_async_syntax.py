bc40690a1dfc75cb2061caf1926606f7d8ed579a
faykris <faykris28@gmail.com>
2022-02-01 14:48:58 -0500

    [Update] 0-basic_async_syntax.py: add union with float

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,9 +2,10 @@
 """0. The basics of async"""
 import random
 import time
+from typing import Union
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_random(max_delay: Union[int, float] = 10) -> float:
     """wait_random - make delay in range of max_delay"""
     delay = random.uniform(0, max_delay)
     time.sleep(delay)
