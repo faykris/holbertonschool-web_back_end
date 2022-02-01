b85e419805f7c9b0a0982b66fc2d73b021caf6c1
faykris <faykris28@gmail.com>
2022-02-01 15:39:38 -0500

    [Add] 1-concurrent_coroutines.py: validate with checker

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""1. Let's execute multiple coroutines at the same time with async"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    wList = []
+    for _ in range(n):
+        wList.append(await wait_random(max_delay))
+
+    return wList
