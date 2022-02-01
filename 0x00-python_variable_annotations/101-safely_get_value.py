7268a3eb62a105a7f55e5ca3cb71d8ed7c78b8b5
faykris <faykris28@gmail.com>
2022-01-31 21:10:23 -0500

    [Update] 101-safe_get_value.py: trying fix PEP8

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """10. Duck typing - first element of a sequence"""
-from typing import Iterable, List, Sequence, Tuple, Union, Any, Mapping, TypeVar
+from typing import Union, Any, Mapping, TypeVar
 
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """returns a dict or a default value (type variable or None)"""
     if key in dct:
         return dct[key]
