bbfbd2d56c54267ea8b27d656ed5e78844189e8c
faykris <faykris28@gmail.com>
2022-02-01 15:44:24 -0500

    [Add] 1-concurrent_coroutines.py: fix documentation and add sort() in list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,8 +7,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n - make delays as much as n value"""
     wList = []
     for _ in range(n):
         wList.append(await wait_random(max_delay))
 
-    return wList
+    return wList.sort()
