c65c3035131ff28ed5129b1a2082a95aae0b0c80
faykris <faykris28@gmail.com>
2022-02-01 15:48:11 -0500

    [Add] 1-concurrent_coroutines.py: fix return wList

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """1. Let's execute multiple coroutines at the same time with async"""
 import asyncio
+from tkinter import W
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -11,5 +12,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     wList = []
     for _ in range(n):
         wList.append(await wait_random(max_delay))
-
-    return wList.sort()
+    wList.sort()
+    return wList
