21520ed0effef505351aa0af45fd5bcee1666c62
faykris <faykris28@gmail.com>
2022-02-01 17:23:56 -0500

    [Add] 2-measure_runtime.py: validate what check is

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -13,4 +13,4 @@ def measure_time(n: int, max_delay: int) -> float:
     s = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     total_time = time.perf_counter() - s
-    return total_time / n
+    return total_time
