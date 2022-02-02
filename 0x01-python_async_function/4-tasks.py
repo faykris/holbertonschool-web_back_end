3064109a2caf393e3a8bb19ae814e911df1f7ce2
faykris <faykris28@gmail.com>
2022-02-01 22:22:34 -0500

    [Update] 4-tasks.py: trying change iterator _ by i

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """task_wait_n - make delays as much as n value"""
     wList = []
-    for _ in range(n):
+    for i in range(n):
         wList.append(await task_wait_random(max_delay))
-
     return sorted(wList)
