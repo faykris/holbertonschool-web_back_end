b25cc9487f98a576e8a5fc66a11e3d4c97d437a4
faykris <faykris28@gmail.com>
2022-02-02 21:32:14 -0500

    [Update] 1-async_comprehension.py: trying add type annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """0. Async Generator"""
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """ return the 10 random numbers using async_generator"""
     return [i async for i in async_generator()]
