e46fb8850751db588e0c317bf3fc926821d1074f
faykris <faykris28@gmail.com>
2022-02-04 18:31:10 -0500

    [Update] 4-task.py: trying with sorting

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """task_wait_n - make delays as much as n value"""
-    wList = []
-    for i in range(n):
-        wList.append(await task_wait_random(max_delay))
-    return wList
+    delays_list = []
+    for _ in range(n):
+        delays_list.append(await task_wait_random(max_delay))
+    return sorted(delays_list)
