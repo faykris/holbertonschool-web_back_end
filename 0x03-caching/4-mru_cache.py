a87dcb8635f63a5eb3a3c333ddd3e727561adbbc
faykris <faykris28@gmail.com>
2022-02-04 18:25:19 -0500

    [Add] 4-mru_cache.py: validate with checker

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""4. MRU Caching"""
+
+from base_caching import BaseCaching
+import datetime
+
+
+class MRUCache(BaseCaching):
+    """MRUCache - caching system class """
+    action_keys = {}
+    keys_list = []
+
+    def __init__(self):
+        """constructor method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """put - assign dictionary key and their item"""
+        if key is None or item is None:
+            pass
+        else:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.action_keys[key] = datetime.datetime.now()
+            elif len(self.cache_data) + 1 > self.MAX_ITEMS:
+                most_recent = max(self.action_keys, key=self.action_keys.get)
+                self.cache_data.pop(most_recent)
+                self.action_keys.pop(most_recent)
+                print("DISCARD: {}".format(most_recent))
+            self.cache_data[key] = item
+            self.action_keys[key] = datetime.datetime.now()
+
+    def get(self, key):
+        """get - return value in cache linked to key"""
+        if key is not None and key in self.cache_data:
+            self.action_keys[key] = datetime.datetime.now()
+            return self.cache_data[key]
+        else:
+            return None
