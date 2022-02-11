2997e6779b5536bcebcec5fd6e89326888fb0dd3
faykris <faykris28@gmail.com>
2022-02-11 18:21:48 -0500

    [Update] task 6, encrypt_password.py: validate with checker

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -6,3 +6,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """hash_password - returns hashed password in byte string"""
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    return bcrypt.checkpw(password.encode('UTF-8'), hashed_password)
