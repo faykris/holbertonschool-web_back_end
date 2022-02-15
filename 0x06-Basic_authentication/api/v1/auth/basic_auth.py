27d9f909cdf3c1798b9e4df9ef81577770714878
faykris <faykris28@gmail.com>
2022-02-15 16:55:44 -0500

    [Update] basic_auth.py: validate task 9 with checker

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,12 @@ class BasicAuth(Auth):
             return message
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        if decoded_base64_authorization_header is None or \
+                type(decoded_base64_authorization_header) != str or \
+                ':' not in decoded_base64_authorization_header:
+            return None
+        credentials = decoded_base64_authorization_header.split(':')
+        return tuple(credentials)
