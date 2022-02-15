f060260627d74c6e5d42764b87bdd087e03f7986
faykris <faykris28@gmail.com>
2022-02-15 14:36:47 -0500

    [Update] basic_auth.py: validate task 7 with checker

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth - empty class by now"""
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        if authorization_header is None or \
+                type(authorization_header) != str or \
+                authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header.split()[1]
