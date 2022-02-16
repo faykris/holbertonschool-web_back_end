f5dbdc09ec90df3b101c276aa4c3dff33063126d
faykris <faykris28@gmail.com>
2022-02-15 20:53:16 -0500

    [Update] basic_auth.py: task 11 - validate with checker

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,13 @@ class BasicAuth(Auth):
             return None
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user - return a user object using request"""
+
+        a = self.authorization_header(request)
+        auth_header = self.extract_base64_authorization_header(a)
+        decoded_header = self.decode_base64_authorization_header(auth_header)
+        user_cred = self.extract_user_credentials(decoded_header)
+        user_obj = self.user_object_from_credentials(user_cred[0], user_cred[1])
+        return user_obj
