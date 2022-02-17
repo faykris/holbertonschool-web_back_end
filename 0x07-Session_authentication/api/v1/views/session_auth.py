546523e568e9491b4a0f89095094676811a53713
faykris <faykris28@gmail.com>
2022-02-17 12:34:44 -0500

    [Update] session_auth.py: verify with checker

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -5,17 +5,17 @@
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models.user import User
+from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session_login() -> str:
     """ POST /api/v1/auth_session/login
     Return:
-      - session
+      - session response json with cookie
     """
     email = request.form.get("email")
     password = request.form.get("password")
-
     if not email:
         return jsonify({"error": "email missing"}), 400
     if not password:
@@ -23,12 +23,13 @@ def auth_session_login() -> str:
     obj_list = User.search({"email": email})
     if not obj_list:
         return jsonify({"error": "no user found for this email"}), 404
-    if User.is_valid_password(obj_list, password):
-        for i in range(len(obj_list)):
-            if User.is_valid_password(obj_list[i], password):
-                """
-                from api.v1.app import auth
-                session_id = auth.create_session(obj_list[i].id)
-                return obj_list[i]
-                """
-        return jsonify({"error": "wrong password"}), 401
+    for i in range(len(obj_list)):
+        if User.is_valid_password(obj_list[i], password):
+            from api.v1.app import auth      # WARNING with circular import
+            session = jsonify(obj_list[i].to_json())
+            session.set_cookie(
+                getenv("SESSION_NAME", None),
+                auth.create_session(obj_list[i].id)
+            )
+            return session
+    return jsonify({"error": "wrong password"}), 401
