aa330fcfdf305ae3d8ffcea7b5318a551f374a3f
faykris <faykris28@gmail.com>
2022-02-20 15:40:13 -0500

    [Update] session_exp_auth.py, app.py: fix if session error forbidden

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module of session database auth
+""" Module of database session expiration
 """
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
@@ -10,40 +10,40 @@ class SessionDBAuth(SessionExpAuth):
     """SessionDBAuth - class"""
 
     def create_session(self, user_id=None):
-        """create_session - method"""
+        """Create session with corresponding user_id"""
         if not user_id:
             return None
         session_id = super().create_session(user_id)
         if not session_id:
             return None
-        user_session = UserSession(user_id=user_id, session_id=user_id)
+        user_session = UserSession(user_id=user_id, session_id=session_id)
         user_session.save()
         UserSession.save_to_file()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """user_id_for_session_id - method"""
-        if session_id is None:
+        """user_id_for_session_id - returns user_id from session"""
+        if not session_id:
             return None
         UserSession.load_from_file()
         users = UserSession.search({'session_id': session_id})
         for user in users:
-            if user.create_at + timedelta(
+            if user.created_at + timedelta(
                     seconds=self.session_duration) < datetime.now():
                 return None
             return user.user_id
 
     def destroy_session(self, request=None):
-        """destroy_session - method"""
+        """destroy_session - logout session"""
         if not request:
             return False
         session_id = self.session_cookie(request)
         if not session_id:
             return False
-        if self.user_id_by_session_id.get(session_id):
-            users = UserSession.search({'session_id': session_id})
-            for user in users:
-                user.remove()
-                UserSession.save_to_file()
-                return True
-        return False
+        if not self.user_id_for_session_id(session_id):
+            return False
+        users = UserSession.search({'session_id': session_id})
+        for user in users:
+            user.remove()
+            UserSession.save_to_file()
+        return True
