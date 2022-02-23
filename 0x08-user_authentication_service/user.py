cbd4d2566a9619e689c5488000d5bf3fedd45f14
faykris <faykris28@gmail.com>
2022-02-23 13:45:54 -0500

    [update] user.py: add value 250 in string fields

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -14,14 +14,14 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
     def __repr__(self) -> str:
         return f"<User(id={self.id}, " + \
             f"email='{self.email}', " + \
             f"hashed_password='{self.hashed_password}', " + \
             f"session_id='{self.hashed_password}', " + \
-            f"reset_token='{self.reset_token}')>'"
+            f"reset_token='{self.reset_token}')>"
