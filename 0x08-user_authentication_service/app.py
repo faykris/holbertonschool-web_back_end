5954b9e54f8b604d3d089056d3e97f9654cacab7
faykris <faykris28@gmail.com>
2022-02-24 13:54:29 -0500

    [Update] app.py: add register user function

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,6 +3,9 @@
 Route module for the API
 """
 from flask import Flask, jsonify, abort, request
+from auth import Auth
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +13,20 @@ app = Flask(__name__)
 def welcome() -> str:
     """welcome - test"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """register_user"""
+    try:
+        user = AUTH.register_user(
+            request.form['email'],
+            request.form['password']
+        )
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except:
+        ValueError
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
