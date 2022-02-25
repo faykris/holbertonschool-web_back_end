0f73046f9dece6693d6414d3b057a370e72c31fb
faykris <faykris28@gmail.com>
2022-02-25 16:02:33 -0500

    [Update] app.py: verify user_profile

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -53,5 +53,18 @@ def logout() -> str:
         abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def user_profile() -> str:
+    """user_profile"""
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
