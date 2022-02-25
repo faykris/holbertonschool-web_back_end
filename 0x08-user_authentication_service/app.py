cd8b03a8767c76541ddfd93f4c8c26f0af3e86c0
faykris <faykris28@gmail.com>
2022-02-25 17:08:23 -0500

    [Update] app.py: verify update_password

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -77,8 +77,8 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route('/reset_password', methods=['PUT'])
-def update_password() -> str:
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
     """update_password"""
     try:
         email = request.form.get('email')
