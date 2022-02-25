5616a1ecf5a8c6c6f7dbb33087f0c073e038b4f9
faykris <faykris28@gmail.com>
2022-02-25 17:00:09 -0500

    [Update] auth.py: verify update_password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -81,3 +81,13 @@ class Auth:
             return new_uuid
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=hashed_password,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
