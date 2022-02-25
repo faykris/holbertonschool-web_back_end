3483dd7309dbdb7e72ef6007e17ce8d64761b532
faykris <faykris28@gmail.com>
2022-02-25 15:11:44 -0500

    [Update] db.py: trying with typevar in create_user method too

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """add_user"""
         user = User()
         user.email = email
