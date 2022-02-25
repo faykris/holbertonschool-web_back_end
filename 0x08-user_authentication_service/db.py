67ff926479f37a56f41d2656f33f54466c9876b2
faykris <faykris28@gmail.com>
2022-02-25 15:24:12 -0500

    [Update] db.py: trying changing order of process

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -46,9 +46,9 @@ class DB:
         """find_user_by"""
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
-            if user:
-                return user
-            raise NoResultFound
+            if user is None:
+                raise NoResultFound
+            return user
         except TypeError:
             raise InvalidRequestError
 
