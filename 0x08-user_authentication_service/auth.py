edc1edac44c6a535f01c44d7b5999fd4b97bc027
faykris <faykris28@gmail.com>
2022-02-25 18:34:02 -0500

    [Update] app.py: verify update_password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -86,9 +86,8 @@ class Auth:
         """update_password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed = _hash_password(password)
             self._db.update_user(user.id,
-                                 hashed_password=hashed,
+                                 hashed_password=_hash_password(password),
                                  reset_token=None)
         except Exception:
             raise ValueError
