481a6a08961da8cbcaa2b6517abe7ee909ba553d
faykris <faykris28@gmail.com>
2022-02-25 15:46:47 -0500

    [Update] auth.py: verify destroy session

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -67,3 +67,7 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """destroy_session"""
+        self._db.update_user(user_id, session_id=None)
