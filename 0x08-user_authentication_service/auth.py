89d954fb00c0d02060760d83a4cb2dff5f0b516c
faykris <faykris28@gmail.com>
2022-02-25 17:01:36 -0500

    [Update] auth.py: change by Exception

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -83,11 +83,12 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
+        """update_password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = _hash_password(password)
+            hashed = _hash_password(password)
             self._db.update_user(user.id,
-                                 hashed_password=hashed_password,
+                                 hashed_password=hashed,
                                  reset_token=None)
-        except NoResultFound:
+        except Exception:
             raise ValueError
