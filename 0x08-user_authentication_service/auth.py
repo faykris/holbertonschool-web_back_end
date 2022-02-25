3254f489288b839b5c9a07951c82d29929b70bb5
faykris <faykris28@gmail.com>
2022-02-25 13:30:52 -0500

    [Update] auth.py: validate create_session method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -49,3 +49,13 @@ class Auth:
             return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """create_session"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return user.session_id
+        except NoResultFound:
+            return None
