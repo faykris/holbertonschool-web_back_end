5f7c6b3ebc6c2d629c806f03722aa2a7b83f923b
faykris <faykris28@gmail.com>
2022-02-25 19:21:15 -0500

    [Update] app.py: verify update_password

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -77,12 +77,12 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route('/reset_password', methods=['PUT'])
-def update_password():
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
     """update_password"""
     try:
         email = request.form['email']
-        reset_token = request.form['request_token']
+        reset_token = request.form['reset_token']
         new_password = request.form['new_password']
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
