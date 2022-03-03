780fe89b94e5e212f3b9bf006ddcf6f49dfd5ab9
faykris <faykris28@gmail.com>
2022-03-03 12:12:49 -0500

    [update] 0-app.py: vadd run and return and route method

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -8,8 +8,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def status() -> str:
-    """ GET 
+    """ GET
     Return:
       - the status of the API
     """
-    pass
+    return
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
