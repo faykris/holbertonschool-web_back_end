6aa2640788986803902bbb401387e7fd370145ff
faykris <faykris28@gmail.com>
2022-03-02 22:44:43 -0500

    [Update] test_client.py: add test_org

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -3,11 +3,22 @@
 """
 import unittest
 from unittest.mock import patch
-from client import GithubOrgClient
+from client import GithubOrgClient, get_json
 from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """TestGithubOrgClient - class"""
 
-    pass
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, m_json):
+        """ Test method returns correct output """
+        gh_client = GithubOrgClient(org_name)
+        gh_client.org()
+        m_json.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(
+                org_name))
