6eb14af0df45bd5b342b129591f658a6d2760c24
faykris <faykris28@gmail.com>
2022-03-03 16:49:45 -0500

    [update] 1-app.py: uncomment dafult locale in class

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -12,7 +12,7 @@ babel = Babel(app)
 class Config(object):
     """Config - class"""
     LANGUAGES = ["en", "fr"]
-    # BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_LOCALE = "en"
     # BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
