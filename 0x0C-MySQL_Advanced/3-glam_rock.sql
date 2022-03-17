c4e830369b8ff64f21abc6d988b6465e32c977c8
faykris <faykris@gmail.com>
2022-03-17 16:17:01 +0000

    [Update]2-fans.sql: fix second column name

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,7 @@
 -- 3. Old school band
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 
-
+CREATE PROCEDURE years (IN band VARCHAR(255), OUT lifespan INT)
+BEGIN
+	SELECT COUNT(*) FROM metal_bands WHERE style LIKE '%"@band"%'
+END
