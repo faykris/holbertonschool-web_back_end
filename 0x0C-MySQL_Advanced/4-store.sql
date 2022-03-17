7f81cba26731fc1199d26e3e35092cc49860da10
faykris <faykris@gmail.com>
2022-03-17 23:12:59 +0000

    [Add] 4-store.sql: validate with checker

---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- 4. Buy buy buy
+-- Trigger that decreases the quantity of an item after adding a new order.
+
+CREATE TRIGGER decrease_quantity_item
+AFTER INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
