1d5be92c45e3dc3088f1b8ddb22a6a0db323d450
faykris <faykris@gmail.com>
2022-03-18 00:41:07 +0000

    [Add] 5-valid_email.sql: validate with checker

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,17 @@
+-- 5. Email validation to sent
+-- Trigger that resets the attribute valid_email only when the email has been changed.
+
+DELIMITER $$
+
+CREATE TRIGGER change_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF OLD.email <> NEW.email THEN
+SET NEW.valid_email = 0;
+END IF;
+END
+
+$$
+
+DELIMITER ;
