83575c97a59a0ea4705b5e7327575063b61ecd79
faykris <faykris28@gmail.com>
2022-03-24 14:50:51 -0500

    [Add] 101-iterateThroughObject.js: verify with checker

---
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,5 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
+  for (const employee of reportWithIterator) employees.push(employee);
+  return employees.join(' | ');
+}
