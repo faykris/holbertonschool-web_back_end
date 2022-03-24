069a80f99506bb36b5806bb9992d475ab0a6c49e
faykris <faykris28@gmail.com>
2022-03-24 09:21:18 -0500

    [Update] 1-block-scoped.js: unable ESLint inside if condition

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    /*eslint-disable */
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
