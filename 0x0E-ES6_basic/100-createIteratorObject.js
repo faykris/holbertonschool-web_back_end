027844382e62e4edc65575cbd0bab14819e24524
faykris <faykris28@gmail.com>
2022-03-24 14:39:32 -0500

    [Add] 100-createIteratorObject.js: verify with checker

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,9 @@
 export default function createIteratorObject(report) {
-  let iterable = [];
-
-  iterable = {
+  return {
     * [Symbol.iterator]() {
       for (const employee of Object.values(report.allEmployees)) {
         for (const i of employee) yield i;
       }
     },
   };
-
-  return iterable;
 }
