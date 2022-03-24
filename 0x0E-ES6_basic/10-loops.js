ca28b297b58a9226009f9583c6450e41ae89e14b
faykris <faykris28@gmail.com>
2022-03-23 23:49:05 -0500

    [Add] 0-12 task files: incomplete 10-12

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
+  }
+  return newArray;
+}
---
+++ 0x0E-ES6_basic/README.md
@@ -0,0 +1,2 @@
+# 0x0E. ES6 Basics
+This directory contains the required file to solve each task of this project.
