c6cff42363cdbecacc9d5ea9744940cea636fe17
faykris <faykris28@gmail.com>
2022-03-24 09:52:16 -0500

    [Update] 12-createReportObject.js: add spread syntax in employeesList and Object.keys to count with length

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,3 +1,6 @@
 export default function createReportObject(employeesList) {
-  return { allEmployees: employeesList, getNumberOfDepartments() { return employeesList.length; } };
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) { return Object.keys(employeesList).length; },
+  };
 }
