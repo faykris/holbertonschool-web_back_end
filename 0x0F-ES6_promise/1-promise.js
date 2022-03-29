70760067f4d9b994306d12608462b70448087031
faykris <faykris28@gmail.com>
2022-03-29 09:49:48 -0500

    [Update] 1-promise.js: change indentation by 2 spaces, add Boolean in function parameter

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
-export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success',
-            });
-        }
-        else {
-            reject('The fake API is not working currently');
-        }
-    });
+export default function getFullResponseFromAPI(success = Boolean) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    }
+    else {
+      reject('The fake API is not working currently');
+    }
+  });
 }
