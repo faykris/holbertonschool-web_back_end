3d4be87e5ce60ad19b9a303fa73df76ca6ff3a2b
faykris <faykris28@gmail.com>
2022-03-29 16:39:38 -0500

    [Update] 3-all.js: fix indentation to 2 spaces

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((res) => {
-    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   })
       .catch(() => console.log('Signup system offline'));
 }
