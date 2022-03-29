6920ae14ea10ab9e998e67f8fc412108c830d113
faykris <faykris28@gmail.com>
2022-03-29 15:11:50 -0500

    [Update] 2-then.js: change by then, catch error with Error, add finally to message

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,17 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-        if (promise instanceof Promise) {
-            resolve({
-                status: 200,
-                body: 'Success',
-            });
-        }
-        else {
-            reject();
-        }
-    }).then(res => {
-        console.log("Got a response from the API");
-    }).catch(err => {
-        console.log("Got a response from the API");
-    });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
