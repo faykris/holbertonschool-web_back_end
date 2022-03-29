162e6a3b21d846892981185aea34a717d3e7820e
faykris <faykris28@gmail.com>
2022-03-29 16:49:41 -0500

    [Update] 4-user-promise.js: fix indentation to 2 spaces

--- 0x0F-ES6_promise/4-user-promise.js
+++ 0x0F-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve(
-        {
-            firstName,
-            lastName,
-        }
-    );
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    }
+  );
 }
