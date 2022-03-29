3c7992ce950b703698d8ad00ab991780e35c2f84
faykris <faykris28@gmail.com>
2022-03-29 17:06:00 -0500

    [Update] 5-photo-reject.js: change all by .reject with instance of Error

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-    return new Promise((resolve, reject) => {
-        reject(`${filename} cannot be processed`);
-    });
+  return Promise.reject(
+    new Error(`${filename} cannot be processed`),
+  );
 }
