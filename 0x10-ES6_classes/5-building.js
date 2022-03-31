70f60072e233be53cde94fa465b002b04d7fbf48
faykris <faykris28@gmail.com>
2022-03-31 15:08:39 -0500

    [Add] 5-building.js: verify with checker

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building
+      && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
