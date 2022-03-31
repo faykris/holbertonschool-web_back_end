e33c9b6d059d00467c0707a4930a0eaf4c2fd215
faykris <faykris28@gmail.com>
2022-03-31 17:05:02 -0500

    [update] 100-evcar.js: fix order constructor args

--- 0x10-ES6_classes/100-evcar.js
+++ 0x10-ES6_classes/100-evcar.js
@@ -2,7 +2,7 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, color, motor);
+    super(brand, motor, color);
     this._range = range;
   }
 
