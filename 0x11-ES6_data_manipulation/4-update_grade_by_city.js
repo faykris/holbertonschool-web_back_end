5a156dc43f2aeac888b915d10f2dca99de9ab341
faykris <faykris28@gmail.com>
2022-04-05 16:19:43 -0500

    [Update] 4-update_grade_by_city.js: verify with checker

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,6 +2,15 @@ export default function updateStudentGradeByCity(list, city, newGrades) {
   return list.filter(
     (student) => student.location === city,
   ).map(
-    (student) => newGrades.filter((grade) => grade.id === student.id),
+    (student) => {
+      const std = student;
+      const grades = newGrades.filter((grade) => grade.studentId === student.id);
+      if (grades.length > 0) {
+        std.grade = grades[0].grade;
+      } else {
+        std.grade = 'N/A';
+      }
+      return student;
+    },
   );
 }
