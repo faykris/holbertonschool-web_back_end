64bd6a6b0d4c46283fae81631b73b0c849a3829d
faykris <faykris28@gmail.com>
2022-04-05 19:13:25 -0500

    [Update] 8-clean_set.js: add if a string condition

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let text = '';
   const array = [];
 
-  if (startString && typeof set === 'object') {
+  if (startString && typeof startString === 'string') {
     for (const element of set) {
       if (element && element.startsWith(startString)) {
         array.push(element.slice(startString.length));
