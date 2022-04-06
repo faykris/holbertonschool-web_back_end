a4775af9dd3341f1e18f2de168f7ec6eca293b6a
faykris <faykris28@gmail.com>
2022-04-05 20:19:54 -0500

    [Add] 10-update_uniq_items.js: verify with checker

---
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,5 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const [key] of map) if (map.get(key) === 1) map.set(key, 100);
+  return map;
+}
