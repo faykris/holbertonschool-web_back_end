9a3ec209a9cc12b6ff49079a56c017413db95718
faykris <faykris28@gmail.com>
2022-04-09 15:53:50 -0500

    [Update] 4-http.js: remove unnecessary server parameters

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,13 +1,5 @@
 const http = require('http');
 
-const hostname = 'localhost';
-const port = 1245;
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-});
-
-server.listen(port, hostname, () => {
-});
+}).listen(1245);
