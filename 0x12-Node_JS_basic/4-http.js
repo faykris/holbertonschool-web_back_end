d20c88d1e9eb0be97744cf441db160dc70236a28
faykris <faykris28@gmail.com>
2022-04-09 15:56:32 -0500

    [Update] 4-http.js: add module.exports

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,5 +1,5 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+module.exports = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 }).listen(1245);
