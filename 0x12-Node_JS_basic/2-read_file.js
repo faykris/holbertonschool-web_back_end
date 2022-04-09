7a15d7a43f97a2151132cb237f09c3b77f8e1487
faykris <faykris28@gmail.com>
2022-04-09 16:23:55 -0500

    [Update] 3-read_file.js: change readFile by readFilSync and heir logical

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  fs.readFile(path, { encoding: 'utf8', flag: 'r' },
-    (err, data) => {
-      if (err) throw new Error('Cannot load the database');
-      else {
-        const lines = data.split('\n');
-        lines.splice(0, 1);
-        for (let i = lines.length - 1; i >= 0; i -= 1) {
-          if (lines[i].length === 0) lines.splice(i, 1);
-        }
-        const number = lines.length;
-        const obj = {};
-        for (const line of lines) {
-          const row = line.split(',');
-          if (!obj[row[3]]) obj[row[3]] = [];
-          obj[row[3]].push(row[0]);
-        }
-        if (number > 0) {
-          console.log(`Number of students: ${number}`);
-          for (const key of Object.keys(obj)) {
-            console.log(`Number of students in ${key}: ${obj[key].length}. List: ${obj[key].join(', ')}`);
-          }
-        }
-      }
-    });
+  let data;
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+  const lines = data.split('\n');
+  lines.splice(0, 1);
+  for (let i = lines.length - 1; i >= 0; i -= 1) {
+    if (lines[i].length === 0) lines.splice(i, 1);
+  }
+  const number = lines.length;
+  const obj = {};
+  for (const line of lines) {
+    const row = line.split(',');
+    if (!obj[row[3]]) obj[row[3]] = [];
+    obj[row[3]].push(row[0]);
+  }
+  if (number > 0) {
+    console.log(`Number of students: ${number}`);
+    for (const key of Object.keys(obj)) {
+      console.log(`Number of students in ${key}: ${obj[key].length}. List: ${obj[key].join(', ')}`);
+    }
+  }
 };
