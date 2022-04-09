31c9f15bdb58006ac40d5f7390a54434ffbb8493
faykris <faykris28@gmail.com>
2022-04-09 15:37:08 -0500

    [Update] 2-read_file.js: fix ESlint and change for in by for Object.keys()

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,12 +1,13 @@
-module.exports = function countStudents (path) {
-  const fs = require('fs');
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
   fs.readFile(path, { encoding: 'utf8', flag: 'r' },
-    function (err, data) {
+    (err, data) => {
       if (err) throw new Error('Cannot load the database');
       else {
         const lines = data.split('\n');
         lines.splice(0, 1);
-        for (let i = lines.length - 1; i >= 0; i--) {
+        for (let i = lines.length - 1; i >= 0; i -= 1) {
           if (lines[i].length === 0) lines.splice(i, 1);
         }
         const number = lines.length;
@@ -18,7 +19,7 @@ module.exports = function countStudents (path) {
         }
         if (number > 0) {
           console.log(`Number of students: ${number}`);
-          for (const key in obj) {
+          for (const key of Object.keys(obj)) {
             console.log(`Number of students in ${key}: ${obj[key].length}. List: ${obj[key].join(', ')}`);
           }
         }
