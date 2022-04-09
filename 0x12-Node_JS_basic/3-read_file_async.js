124c9f0e0058280fad9499ec039feb2eccf7df50
faykris <faykris28@gmail.com>
2022-04-09 16:07:44 -0500

    [Update] 3-read_file_async.js: fix ESLint

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -5,7 +5,7 @@ module.exports = async function countStudents(path) {
     .then((data) => {
       const lines = data.split('\n');
       lines.splice(0, 1);
-      for (let i = lines.length - 1; i >= 0; i = i - 1) {
+      for (let i = lines.length - 1; i >= 0; i -= 1) {
         if (lines[i].length === 0) lines.splice(i, 1);
       }
       const number = lines.length;
