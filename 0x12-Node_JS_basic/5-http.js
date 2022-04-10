d2b8dcd597b17e1f9aa05e4b1717624a93d0bf3d
faykris <faykris28@gmail.com>
2022-04-10 16:09:40 -0500

    [Update] 5-http.js: Change default CSV file by argument

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -2,13 +2,14 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.write('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students');
     try {
-      const obj = await countStudents('database.csv');
+      const obj = await countStudents(process.argv[2]);
       let number = 0;
       for (const ele of Object.values(obj)) number += ele.length;
       res.write(`\nNumber of students: ${number}`);
