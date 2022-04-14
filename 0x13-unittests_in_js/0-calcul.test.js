fd098989c627caf75c43b4da7c2ea8b7b3841de3
faykris <faykris28@gmail.com>
2022-04-14 12:15:49 -0500

    [Add] 0-calcul.test.js,trying only one description

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -7,16 +7,10 @@ describe('calculateNumber', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('checks negative numbers', function() {
-    assert.equal(calculateNumber(-1, -3), -4);
-    assert.equal(calculateNumber(-1, -3.7), -4);
-    assert.equal(calculateNumber(-1.2, -3.7), -4);
-    assert.equal(calculateNumber(-1.5, -3.7), -5);
-  });
-
-  it('checks combined numbers', function() {
+    assert.equal(calculateNumber(-1, 3), 2);
+    assert.equal(calculateNumber(1, -3.7), -2);
+    assert.equal(calculateNumber(0, -3.7), -3);
+    assert.equal(calculateNumber(-1.5, 0), -1);
     assert.equal(calculateNumber(-1, 3), 2);
     assert.equal(calculateNumber(1, -3.7), -2);
     assert.equal(calculateNumber(0, -3.7), -3);
